19519e06c5ce78d8dd4908fd30fc24e1bcdf0f98
rittschapp <philip@rittscher.com>
2024-07-27 18:12:19 -0700

    one time change to category labels plus:  - error handling on info retrieval per url  - fix for dateAdded column  - corrected filter for new bookmarks  - fix error when siteIcon unavailable  - output changes

--- README.md
+++ README.md
@@ -5,3 +5,13 @@ s
 
 must start train - ai / software
 
+Improvements:
+
+A. Cloudflare 403 errors
+    1. on error allow for input of new site info
+    2. handle cloudflare errors (see https://www.zenrows.com/blog/cloudflare-403-forbidden-bypass#web-scraping-api)
+    3. do chat gpt live browsing get around the error? (No!) 
+
+
+
+
--- scripts/process_bookmarks.py
+++ scripts/process_bookmarks.py
@@ -18,136 +18,104 @@ incomingBookmarksFile = "../data/bookmarks.json"
 existingBookmarksFile = "../data/bookmarks.db.json"
 
 ''' categories - need to break this out some more to include security '''
-promptString = "Category [A] AI/ML, [S] Software, [X] Security, [D] DELETE, [O] Other [Q] Quit):> [A] "
-categories = ["A", "S", "X", "O", "D"]
+promptString = "Category [A] AI/ML, [F] Software, [E] Security, [D] DELETE, [O] Other [Q] Quit):> [A] "
+# 'X' is internal error while trying to retrieve link info - to be managed manually later
+categories = ["A", "F", "E", "O", "D", "X"]
 
 
 def main():
-
     incomingBookmarks = loadIncomingBookmarks()
-
     existingBookmarks = loadExistingBookmarks(incomingBookmarks)
+    print(f"Incoming: {incomingBookmarks.shape}")
+    print(f"Existing: {existingBookmarks.shape}")
 
     # exclude not new
     newBookmarks = incomingBookmarks[~incomingBookmarks['key'].isin(existingBookmarks['key'].values)]
     if not newBookmarks.empty:
-        print(f"Found {len(newBookmarks)} new bookmarks to add")
-
+        print(f"New: {newBookmarks.shape}")
         # add all new bookmarks to the bookmark set
-        existingBookmarks = pd.concat([existingBookmarks, newBookmarks])
+        existingBookmarks.set_index(['key'], inplace=True)
+        newBookmarks.set_index(['key'], inplace=True)
+        existingBookmarks = pd.concat([newBookmarks, existingBookmarks])
+        existingBookmarks.reset_index(inplace=True)
 
     processBookmarks(existingBookmarks)
 
+    print("Saving results")
+
     # save the changes
     existingBookmarks.to_json(existingBookmarksFile)
-    existingBookmarks.to_json(f"{existingBookmarksFile}.pub", orient='records');
-
-
-
-
-def getTitle(site):
-    try:
-        headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:122.0) Gecko/20100101 Firefox/122.0'}
-        response = requests.get(site, headers=headers, timeout=50)
-
-        tree = fromstring(response.content)
-        return tree.findtext('.//title')
-
-    except Exception as e:
-        print(f"Error retrieving page {site}\n\n{e}")
-    return ''
+    existingBookmarks.to_json(f"{existingBookmarksFile}.pub", orient='records')
 
 
 def processBookmarks(existingBookmarks):
-
     # process all new bookmarks (and any that have not been processed before)
     bookmarksToProcess = existingBookmarks[
         (existingBookmarks['category'].isna()) |
         (existingBookmarks['category'] == 'S')
-    ]
+        ]
 
     existingBookmarks.set_index(['key'], inplace=True)
     bookmarksToProcess.set_index(['key'], inplace=True)
 
     count = 1
     siteInfo = loadExistingSiteInfo(existingBookmarks)
+    try:
+        for i, mark in bookmarksToProcess.iterrows():
+            os.system('cls')
+            print(f"\n\nProcessing {count} of {len(bookmarksToProcess)} bookmarks")
+            print(f"URI: {mark['bmkUri']}")
+            count += 1
+            xmark = 'X'
+            uri = urlparse(mark['bmkUri'])
+            domain = uri.netloc
+            '''  TODO - they always say do not modify things "in place" '''
+            mark['domain'] = domain
 
-    for i, mark in bookmarksToProcess.iterrows():
-        os.system('cls')
-        print(f"\n\nProcessing {count} of {len(bookmarksToProcess)} bookmarks")
-        print(f"URI: {mark['bmkUri']}")
-        count += 1
-        xmark = 'X'
-        uri = urlparse(mark['bmkUri'])
-        domain = uri.netloc
-        '''  TODO - they always say do not modify things "in place" '''
-        mark['domain'] = domain
+            if domain not in siteInfo:
+                xmark = ''
+                try:
+                    siteInfo[domain] = getNewSiteInfo(uri)
+                except requests.exceptions.HTTPError as e:
+                    print(F"Error processing URL {e}")
+                    input("Will be marked as error. Enter to continue:> ")
+                    mark['category'] = 'X'
+                    existingBookmarks.loc[i] = mark
+                    continue
 
-        if domain not in siteInfo:
-            xmark = ''
-            siteInfo[domain] = getNewSiteInfo(uri)
+            mark['siteIcon'] = siteInfo[domain]['siteIcon']
+            mark['siteTitle'] = siteInfo[domain]['siteTitle']
+            mark['articleTitle'] = getTitle(mark['bmkUri'])
 
-        print(type(mark))
+            print(f"Site       : {urlparse(mark['bmkUri']).scheme}://{domain}/ -[{xmark}]")
+            print(f"SiteTitle  : {mark['siteTitle']}")
+            print(f"Title      : {mark['articleTitle']}")
+            print(f"Bookmarked : {datetime.fromtimestamp(int(mark['dateAdded']) / 1000)}")
 
-        mark['siteIcon'] = siteInfo[domain]['siteIcon']
-        mark['siteTitle'] = siteInfo[domain]['siteTitle']
-        mark['articleTitle'] = getTitle(mark['bmkUri'])
+            category = ''
+            while not category:
+                c = input(f"{promptString} ").upper()
 
-        print(f"Site       : {urlparse(mark['bmkUri']).scheme}://{domain}/ -[{xmark}]")
-        print(f"SiteTitle  : {mark['siteTitle']}")
-        print(f"Title      : {mark['articleTitle']}")
-        print(f"Bookmarked : {mark['dateAdded']}")
+                if c == '':
+                    c = 'A'
 
-        category = ''
-        while not category:
-            c = input(f"{promptString} ").upper()
-            if c not in ['', 'A', 'S', 'O', 'Q', 'D']:
-                continue
-            if c == '':
-                c = 'A'
-            category = c
-        if category == 'Q':
-            break
+                if c not in categories and c != 'Q':
+                    continue
 
-        mark['category'] = category
+                category = c
 
-        # update the bookmarks db (in memory) with new values
-        existingBookmarks.loc[i] = mark
+            if category == 'Q':
+                break
+
+            mark['category'] = category
+
+            # update the bookmarks db (in memory) with new values
+            existingBookmarks.loc[i] = mark
+
+    except Exception as e:
+        print(f"Exiting due to error {type(e)}")
 
     existingBookmarks.reset_index(inplace=True)
-
-
-def getNewSiteInfo(uri):
-    print(f"Getting new site info {uri}");
-    domain = uri.netloc
-    site = f"{uri.scheme}://{domain}/"
-    siteInfo = {'siteTitle': getTitle(site), 'siteIcon': ''}
-    try:
-        siteImage = favicon.get(f"https://{domain}")[0]
-        siteInfo['siteIcon'] = siteImage.url
-    except requests.exceptions.ConnectionError as e:
-        print(f"Error retrieving site image {domain}\n\n{e}")
-    return siteInfo
-
-
-def loadExistingSiteInfo(existingBookmarks):
-    # gather the site info for existing bookmarks
-    siteInfo = dict()
-
-    for domain in existingBookmarks['domain']:
-        if domain is None or pd.isna(domain):
-            continue            
-
-        # load site info from an existing record
-        domainInfo = existingBookmarks.loc[existingBookmarks['domain'] == domain].iloc[0]
-
-        siteInfo.update({
-            domain: {
-                'siteTitle': domainInfo['siteTitle'],
-                'siteIcon': domainInfo['siteIcon']
-            }
-        })
-    return siteInfo
 
 
 def loadIncomingBookmarks():
@@ -156,30 +124,13 @@ def loadIncomingBookmarks():
     incomingBookmarks = pd.DataFrame(marks)
     incomingBookmarks = incomingBookmarks.loc[incomingBookmarks['parentName'] == 'mobile']
     incomingBookmarks = incomingBookmarks[['bmkUri', 'title', 'dateAdded']]
-
-    print(f"The string before: {incomingBookmarks.loc[0]['dateAdded']}")
-    #print(f"Type: {incomingBookmarks['dateAdded']}.type")
-
-    incomingBookmarks['dateAdded'] = incomingBookmarks['dateAdded'].astype("int")
-
-    print(f"The string after: {incomingBookmarks.loc[0]['dateAdded']}")
-
-
-
-    print(datetime.fromtimestamp(incomingBookmarks.loc[0]['dateAdded']))
-    
-
-    print(f"The string after: {incomingBookmarks.loc[0]['dateAdded']}")
-    print(datetime.fromtimestamp(incomingBookmarks.loc[0]['dateAdded']))
-
-    exit()
-    
-    
     key = incomingBookmarks['bmkUri'].apply(lambda x: hashlib.sha256(x.encode('UTF-8')).hexdigest())
     incomingBookmarks.insert(0, "key", key)
+
     return incomingBookmarks
 
 
+# load and
 def loadExistingBookmarks(incomingBookmarks):
     marks = None
     if not Path(existingBookmarksFile).exists():
@@ -191,7 +142,7 @@ def loadExistingBookmarks(incomingBookmarks):
     else:
         try:
             marks = json.load(open(existingBookmarksFile, encoding='utf-8'))
-            marks =  pd.DataFrame(marks)
+            marks = pd.DataFrame(marks)
         except json.decoder.JSONDecodeError as e:
             # need to troubleshoot the error, adjust data or code
             print(f"Failed to load existing bookmarks file: {e}")
@@ -199,8 +150,55 @@ def loadExistingBookmarks(incomingBookmarks):
     marks["domain"] = marks["domain"].astype("string")
     categoryType = CategoricalDtype(categories=categories, ordered=True)
     marks["category"] = marks["category"].astype(categoryType)
-    marks['dateAdded'] = marks['dateAdded'].astype('int')
     return marks
+
+
+def getTitle(site):
+    try:
+        headers = {'User-Agent': 'PBR/1.0 (Windows NT 10.0; Win64; x64; rv:122.0) Gecko/20100101 Firefox/122.0'}
+        response = requests.get(site, headers=headers, timeout=50)
+
+        tree = fromstring(response.content)
+        return tree.findtext('.//title')
+
+    except Exception as e:
+        print(f"Error retrieving page {site}\n\n{e}")
+    return ''
+
+
+def getNewSiteInfo(uri):
+    print(f"Getting new site info {uri}")
+    domain = uri.netloc
+    site = f"{uri.scheme}://{domain}/"
+    siteInfo = {'siteTitle': getTitle(site), 'siteIcon': ''}
+    try:
+        siteImage = favicon.get(f"https://{domain}")
+        if len(siteImage) > 0:
+            siteImage = siteImage[0]
+            siteInfo['siteIcon'] = siteImage.url
+    except requests.exceptions.ConnectionError as e:
+        print(f"Error retrieving site image {domain}\n\n{e}")
+    return siteInfo
+
+
+def loadExistingSiteInfo(existingBookmarks):
+    # gather the site info for existing bookmarks
+    siteInfo = dict()
+
+    for domain in existingBookmarks['domain']:
+        if domain is None or pd.isna(domain):
+            continue
+
+            # load site info from an existing record
+        domainInfo = existingBookmarks.loc[existingBookmarks['domain'] == domain].iloc[0]
+
+        siteInfo.update({
+            domain: {
+                'siteTitle': domainInfo['siteTitle'],
+                'siteIcon': domainInfo['siteIcon']
+            }
+        })
+    return siteInfo
 
 
 if __name__ == '__main__':
